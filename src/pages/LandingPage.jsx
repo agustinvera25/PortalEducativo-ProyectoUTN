9d625a4c99290ebd038a40cc6fbb9e5e3abb1711
Agustin Vera <ronedev25@gmail.com>
2023-06-23 19:55:52 -0300

    add: Navbar component layout

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
+import Navbar from "../components/Navbar"
 
 function LandingPage(){
     return(
-        <div>
-            LandingPage
-        </div>
+        <>
+            <Navbar/>
+        </>
     )
 }
 
