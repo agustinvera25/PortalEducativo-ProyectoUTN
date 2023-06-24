c134eb0b68bc5b68bad26c34ae8dce05a76ac402
Agustin Vera <ronedev25@gmail.com>
2023-06-23 21:00:31 -0300

    add: Landing Page Header layout

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
+import Header from "../components/LandingPage/Header"
 import Navbar from "../components/Navbar"
 
 function LandingPage(){
     return(
         <>
             <Navbar/>
+            <Header/>
         </>
     )
 }
