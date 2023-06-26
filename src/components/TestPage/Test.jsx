796d9c22270586c2180f1e0f1a2e345db42470e8
Agustin Vera <ronedev25@gmail.com>
2023-06-26 14:38:35 -0300

    add: test component layout

---
+++ src/components/TestPage/Test.jsx
@@ -0,0 +1,14 @@
+const Test = () => {
+  return (
+    <section className="test-section">
+        <div className="circle"></div>
+        <div className="inicio-test">
+            <h2>¿Estas listo?</h2>
+            <p>¿Preparado? Pídele a tu niño/a a cargo que responda las preguntas del siguiente cuestionario. Al acabar el test, sumaremos los puntos en base a lo respondido y te daremos un veredicto sobre lo peligrosas o seguras que son las prácticas de tu hijo al navegar por Internet.</p>
+            <button className="btn1">Empezar test</button>
+        </div>
+    </section>
+  )
+}
+
+export default Test
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import Test from "../components/TestPage/Test"
 
 const TestPage = () => {
   return (
     <>
         <Navbar />
+        <Test />
         <Footer/>
     </>
   )
