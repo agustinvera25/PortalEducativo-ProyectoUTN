c0c7c42b47364636a74a7bb7f980c539efe8fb33
Agustin Vera <ronedev25@gmail.com>
2023-06-26 15:04:49 -0300

    add: test-homepage component styles

--- src/components/TestPage/Test.jsx
+++ src/components/TestPage/Test.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
+
 const Test = () => {
+    const [visible, setVisible] = useState(false)
+
+    useEffect(()=> setVisible(true), [])
   return (
     <section className="test-section">
-        <div className="circle"></div>
-        <div className="inicio-test">
-            <h2>¿Estas listo?</h2>
-            <p>¿Preparado? Pídele a tu niño/a a cargo que responda las preguntas del siguiente cuestionario. Al acabar el test, sumaremos los puntos en base a lo respondido y te daremos un veredicto sobre lo peligrosas o seguras que son las prácticas de tu hijo al navegar por Internet.</p>
-            <button className="btn1">Empezar test</button>
-        </div>
+      <div className={visible ? 'circle visible' : 'circle'}></div>
+      <div className={visible ? 'inicio-test visible': 'inicio-test'}>
+        <h2>¿Estas listo?</h2>
+        <p>
+          ¿Preparado? Pídele a tu niño/a a cargo que responda las preguntas del
+          siguiente cuestionario. Al acabar el test, sumaremos los puntos en
+          base a lo respondido y te daremos un veredicto sobre lo peligrosas o
+          seguras que son las prácticas de tu hijo al navegar por Internet.
+        </p>
+        <button className="btn1">Empezar test</button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Test
+export default Test;
