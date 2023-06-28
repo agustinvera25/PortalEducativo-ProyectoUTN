8d249575dfca0fbd6d0edd709c24ee27c84de0d5
Agustin Vera <ronedev25@gmail.com>
2023-06-28 00:36:39 -0300

    add: CiberacosoConsejos component styles

--- src/components/AprendamosPage/CiberacosoConsejos.jsx
+++ src/components/AprendamosPage/CiberacosoConsejos.jsx
@@ -5,7 +5,7 @@ import consejo4 from '../../assets/ilustaciones/consejo4.png'
 
 const CiberacosoConsejos = () => {
   return (
-        <section className="CiberacosoConsejos-section">
+        <section className="ciberacosoConsejos-section">
             <h2 className="title">Otros consejos</h2>
             <div className="consejos-container">
                 <div className="consejo">
@@ -13,16 +13,16 @@ const CiberacosoConsejos = () => {
                     <img src={consejo1} alt="amigos y conocidos" />
                 </div>
                 <div className="consejo">
-                    <p>Debemos ser cuidadosos y no compartir o decir cosas que puedan herir a otras personas. Tenemos que ser amables unos con otros en línea y en la vida real. Todos, sin excepción.</p>
                     <img src={consejo2} alt="amigos y conocidos" />
+                    <p>Debemos ser cuidadosos y no compartir o decir cosas que puedan herir a otras personas. Tenemos que ser amables unos con otros en línea y en la vida real. Todos, sin excepción.</p>
                 </div>
                 <div className="consejo">
                     <p>Piensa dos veces antes de publicar o compartir algo en plataformas digitales y no des detalles personales como tu dirección, tu número telefónico o el nombre de tu escuela.</p>
                     <img src={consejo3} alt="amigos y conocidos" />
                 </div>
                 <div className="consejo">
-                    <p>Aprende sobre los ajustes de privacidad de tus aplicaciones favoritas para las redes sociales. Puedes decidir quién puede ver tu perfil, enviarte mensajes o comentar tus publicaciones, informar sobre comentarios, mensajes, fotografías y videos hirientes y bloquear completamente a alguien para que no pueda volver a ver tu perfil ni contactarte.</p>
                     <img src={consejo4} alt="amigos y conocidos" />
+                    <p>Aprende sobre los ajustes de privacidad de tus aplicaciones favoritas para las redes sociales. Puedes decidir quién puede ver tu perfil, enviarte mensajes o comentar tus publicaciones, informar sobre comentarios, mensajes, fotografías y videos hirientes y bloquear completamente a alguien para que no pueda volver a ver tu perfil ni contactarte.</p>
                 </div>
             </div>
         </section>
