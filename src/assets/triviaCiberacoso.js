e1682c814c234a6e096ddd317c569edfd2421189
Agustin Vera <ronedev25@gmail.com>
2023-07-04 15:35:39 -0300

    add: Trivia component and triviaCiberacoso data

---
+++ src/assets/triviaCiberacoso.js
@@ -0,0 +1,82 @@
+export const triviaCiberacoso = [
+    {
+      pregunta: "Una buena contraseña para mi identidad digital sería…",
+      opciones: [
+        { letra: "a", opcion: "Mi fecha de nacimiento." },
+        { letra: "b", opcion: "1234." },
+        { letra: "c", opcion: "Una combinación de letras y números." }
+      ]
+    },
+    {
+      pregunta: "Privacidad de mi contraseña",
+      opciones: [
+        { letra: "a", opcion: "Puedo decir mi contraseña a mis amigos, no pasa nada." },
+        { letra: "b", opcion: "Solo mis padres y yo debemos saber mi contraseña." },
+        { letra: "c", opcion: "Puedo contarle a mis amigos de Fortnite mi contraseña." }
+      ]
+    },
+    {
+      pregunta: "Subo una foto a Instagram de un amigo mío y …",
+      opciones: [
+        { letra: "a", opcion: "Puedo borrarla cuando quiera." },
+        { letra: "b", opcion: "Me costará un poco, pero la puedo eliminar." },
+        { letra: "c", opcion: "Ya no se puede eliminar, la imagen se puede copiar de un servidor a otro, puede ser descargada muchas veces y replicada." }
+      ]
+    },
+    {
+      pregunta: "Redes sociales “Amistad”",
+      opciones: [
+        { letra: "a", opcion: "Acepto a cualquiera que me pida ser su amigo, mientras más amigos mejor." },
+        { letra: "b", opcion: "Es más seguro aceptar la invitación solo de aquellos a los que estoy seguro de conocer." },
+        { letra: "c", opcion: "Es un amigo de un amigo de un amigo de mi primo, lo acepto." }
+      ]
+    },
+    {
+      pregunta: "Tengo 11 años y …",
+      opciones: [
+        { letra: "a", opcion: "Puedo tener mi cuenta en Instagram y Facebook sin problemas." },
+        { letra: "b", opcion: "Puedo abrir mi cuenta de TikTok y ver todos los videos que quiera." },
+        { letra: "c", opcion: "En Argentina no se puede abrir una cuenta en redes sociales sino hasta los 13 años con autorización de los padres." }
+      ]
+    },
+    {
+      pregunta: "En Whatsapp …",
+      opciones: [
+        { letra: "a", opcion: "Soy invisible, puedo decir lo que quiera, nadie sabrá que soy yo." },
+        { letra: "b", opcion: "Lo que diga queda escrito y registrado y soy responsable de todo ello." },
+        { letra: "c", opcion: "Puedo publicar fotos en mis grupos aun sin saber de quienes son." }
+      ]
+    },
+    {
+      pregunta: "En Internet todo el mundo es quien dice ser…",
+      opciones: [
+        { letra: "a", opcion: "Si la gente pone sus datos y su foto siempre dice la verdad." },
+        { letra: "b", opcion: "No, mucha gente inventa su identidad y finge ser quien no es." },
+        { letra: "c", opcion: "Si, solo vas a conocer niños de tu edad." }
+      ]
+    },
+    {
+        pregunta: "Alguien que tiene muchos seguidores me pide ser su amigo…",
+        opciones: [
+          { letra: "a", opcion: "Le doy todos mis datos y me hago su amigo inmediatamente." },
+          { letra: "b", opcion: "Desconfío de la cuenta, podría ser un adulto fingiendo esa identidad." },
+          { letra: "c", opcion: "Le paso fotos para que vea todo lo que hago." }
+        ]
+      },
+      {
+        pregunta: "Me doy cuenta que alguien me está acosando por las redes…",
+        opciones: [
+          { letra: "a", opcion: "Le sigo el juego, yo soy más listo." },
+          { letra: "b", opcion: "Me aíslo, me deprimo y me encierro en mi habitación. Aguanto todo lo que más pueda." },
+          { letra: "c", opcion: "No espero un minuto, le cuento lo que me pasa a mis padres o maestros." }
+        ]
+      },
+      {
+        pregunta: "Alguien me hostiga todos los días y dice mentiras sobre mí...",
+        opciones: [
+          { letra: "a", opcion: "Me cambio de colegio para que no hablen más." },
+          { letra: "b", opcion: "Le digo a mis padres y denuncio ciberbullying." },
+          { letra: "c", opcion: "Aguanto en silencio y solo." }
+        ]
+      }
+]
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,9 @@
+const Trivia = () => {
+  return (
+    <section className="trivia-section">
+        
+    </section>
+  )
+}
+
+export default Trivia
